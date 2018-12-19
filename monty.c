123bde0c911dbcca1471a1e15df12c00970dc204
wescottsharples <wesc@ttsharpl.es>
2018-12-19 18:46:36 +0000

    Program drives the central process

---
+++ monty.c
@@ -0,0 +1,27 @@
+/**
+ * main - opens a monty script file for parsing
+ * @ac: count of arguments
+ * @av: array of arguments
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+
+	parse_file(file);
+	return (EXIT_SUCCESS);
+}
