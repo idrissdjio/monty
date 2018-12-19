67304dca7129eb8cf8daffc5d71c1443cf64ec02
kai-dg <km.commissions@gmail.com>
2018-12-19 19:24:03 +0000

    adding push, pall, create_st

---
+++ opcodes_1.c
@@ -0,0 +1,55 @@
+void push(stack_t **stack, unsigned int nline)
+{
+	stack_t *new;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: stack not found\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_st(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new->next = *stack;
+	new->prev = NULL;
+	new->n = data.data_n;
+}
+
+void pall(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+	(void)nline;
+
+	temp = *stack;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+void create_st(stack_t **stack)
+{
+	stack_t *new = NULL;
+
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->next = NULL;
+	new->n = data.data_n;
+	new->prev = NULL;
+	*stack = new;
+}
