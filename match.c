627d31d4bdc5e0d28e36b80b241950afce3516df
wescottsharples <wesc@ttsharpl.es>
2018-12-19 18:45:47 +0000

    Contains function for matching op to func pointer

---
+++ match.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+void (*get_op_func(line_t line))(stack_t **, unsigned int)
+{
+	unsigned int i;
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	if comment_check(line.content)
+		return (nop);
+
+	while (ops[i].opcode)
+	{
+		if (strcmp(ops[i].opcode, line.content[0]) == 0)
+		{
+			push_check(line);
+			free(line.content);
+			return(ops[i].f);
+		}
+
+		i++;
+	}
+
+	printf("L%d: unknown instruction %s\n", line.number, line.content[0]);
+	free(line.content);
+	exit(EXIT_FAILURE);
+}
