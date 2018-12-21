a04bc2dabe5880340b091fa18bac06213f3b6c32
wescottsharples <wesc@ttsharpl.es>
2018-12-21 01:06:29 +0000

    Functions change mode of push

---
+++ opcodes_mode.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+void addst(stack_t **stack, unsigned int nline)
+{
+	(void)nline;
+	(void)stack;
+
+	arg.flag = 0;
+}
+
+void addqu(stack_t **stack, unsigned int nline)
+{
+	(void)nline;
+	(void)stack;
+
+	arg.flag = 1;
+}
