5c33f229b741b8ebde450239ea599ef4013e2b41
wescottsharples <wesc@ttsharpl.es>
2018-12-21 01:37:49 +0000

    Fix exit failure

--- monty.c
+++ monty.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	parsefile(file);
